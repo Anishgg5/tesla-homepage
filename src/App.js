bf3a39521a376ce57340d5c676687753e46f73ec
thagu anish <thaguanish@Thagus-Air.fios-router.home>
2021-03-30 22:47:31 -0400

    Header + asset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Clever Programmer</h1>
+      <Header/>
+        {/* items => snapping */}
+        {/* items => snapping */}
+        {/* items => snapping */}
+       {/* items => snapping */}
+
+
     </div>
   );
 }
